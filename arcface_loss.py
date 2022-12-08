9e08bdf19c16d0d9642d035d7364ce6cc791fae9
Lorediel <ld1@live.it>
2022-12-08 16:59:54 +0100

    arcface_loss
    arcface loss implementation

---
+++ arcface_loss.py
@@ -0,0 +1,52 @@
+import math
+import torch
+import torch.nn as nn
+from torch.nn import Parameter
+
+
+def cosine_sim(x1: torch.Tensor, x2: torch.Tensor, dim: int = 1, eps: float = 1e-8) -> torch.Tensor:
+    ip = torch.mm(x1, x2.t())
+    w1 = torch.norm(x1, 2, dim)
+    w2 = torch.norm(x2, 2, dim)
+    return ip / torch.ger(w1, w2).clamp(min=eps)
+
+
+class MarginCosineProduct(nn.Module):
+    """Implement of large margin cosine distance:
+    Args:
+        in_features: size of each input sample
+        out_features: size of each output sample
+        s: norm of input feature
+        m: margin
+    """
+    def __init__(self, in_features: int, out_features: int, s: float = 30.0, m: float = 0.40):
+        super().__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.s = s
+        self.m = m
+        self.cos_m = math.cos(m)
+        self.sin_m = math.sin(m)
+        self.weight = Parameter(torch.Tensor(out_features, in_features))
+        nn.init.xavier_uniform_(self.weight)
+    
+    def forward(self, inputs: torch.Tensor, label: torch.Tensor) -> torch.Tensor:
+        #Cosine similarity between x and weights.
+        cosine = cosine_sim(inputs, self.weight)
+        sine = torch.sqrt((1.0 - torch.pow(cosine, 2)).clamp(0, 1))
+        phi = cosine * self.cos_m - sine * self.sin_m
+        one_hot = torch.zeros_like(cosine)
+        #label.view => vettore colonna. 
+        one_hot.scatter_(1, label.view(-1, 1), 1.0)
+        my_cosine_vector = one_hot * phi + (1.0-one_hot) * cosine
+
+        output = self.s * (my_cosine_vector)
+        #output sul quale verrà applicata la cross entropy loss.
+        return output
+    
+    def __repr__(self):
+        return self.__class__.__name__ + '(' \
+               + 'in_features=' + str(self.in_features) \
+               + ', out_features=' + str(self.out_features) \
+               + ', s=' + str(self.s) \
+               + ', m=' + str(self.m) + ')'
